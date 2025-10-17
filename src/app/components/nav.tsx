590180d3cae342f3996a39192c201027d0928086
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-10-18 04:16:47 +0700

    add nav

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+function Nav() {
+    return (
+        <ul>
+            <li><Link href="/">Home</Link></li>
+        </ul>
+    )
+}
+
+export default Nav;
