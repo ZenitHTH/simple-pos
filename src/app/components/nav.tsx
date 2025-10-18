0ff62979d2bd16db51258a49f0ac6a970d8ea325
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-10-18 18:11:01 +0700

    add navmenu but unfinished

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import Link from "next/link";
 
+interface itemNavInf {
+  link: string;
+  name: string;
+}
+const itemNav: itemNavInf[] = [
+  {
+    link: "/",
+    name: "home",
+  },
+  {
+    link: "/",
+    name: "about",
+  },
+];
+
 function Nav() {
-    return (
-        <ul>
-            <li><Link href="/">Home</Link></li>
-        </ul>
-    )
+  return (
+    <nav className="flex items-center justify-between bg-gray-800">
+      {itemNav.map((item: itemNavInf) => {
+        return (
+          <div className="mx-3" key={0}>
+            <button className="">
+              <Link href={item.link}>{item.name}</Link>
+            </button>
+          </div>
+        );
+      })}
+    </nav>
+  );
 }
 
 export default Nav;
