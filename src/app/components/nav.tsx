01c3719b45fb300d9883955663d2da20b90c0fc1
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-10-19 16:25:48 +0700

    add css.

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,28 +2,48 @@ import React from "react";
 import Link from "next/link";
 
 interface itemNavInf {
+  id: number;
   link: string;
   name: string;
 }
 const itemNav: itemNavInf[] = [
   {
+    id: 0,
     link: "/",
     name: "home",
   },
   {
+    id: 1,
     link: "/",
     name: "about",
+  },
+  {
+    id: 2,
+    link: "/",
+    name: "setting",
+  },
+  {
+    id: 3,
+    link: "/",
+    name: "manage",
+  },
+  {
+    id: 4,
+    link: "/",
+    name: "table",
   },
 ];
 
 function Nav() {
   return (
-    <nav className="flex items-center justify-between bg-gray-800">
+    <nav className="flex items-center justify-between flex-warp bg-gray-800 p-4">
       {itemNav.map((item: itemNavInf) => {
         return (
-          <div className="mx-3" key={0}>
-            <button className="">
-              <Link href={item.link}>{item.name}</Link>
+          <div className="block lg:hidden" key={item.id}>
+            <button className="flex items-center px-4 py-2 border rounded text-teal-200 bg-blue-800 hover:border-white">
+              <Link href={item.link} className="">
+                {item.name}
+              </Link>
             </button>
           </div>
         );
