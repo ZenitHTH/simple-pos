bf97afa976af498358057d06c735b699f65b8eba
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-15 02:52:20 +0700

    add establish connect

--- src-tauri/database/src/lib.rs
+++ src-tauri/database/src/lib.rs
@@ -1,14 +1,22 @@
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+pub mod product;
+
+use crate::product::{model,schema};
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
