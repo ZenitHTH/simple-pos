1eb963ab5f33f12a1bcb0cdd6ee31c19abc7b48f
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-14 18:00:05 +0700

    create / drop receipt

--- src-tauri/database/migrations/2025-11-13-084410-0000_Receipt/up.sql
+++ src-tauri/database/migrations/2025-11-13-084410-0000_Receipt/up.sql
@@ -1,1 +1,9 @@
 -- Your SQL goes here
+CREATE TABLE Receipt (
+id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+receipt_id INTEGER NOT NULL,
+FORGIEN KEY (receipt_id) REFERENCES ReceiptList (receipt_id),
+product_id INTEGER NOT NULL ,
+FORGIEN KEY (product_id) REFERENCES Product (product_id),
+quantity INTEGER NOT NULL
+)
