3403904cfe715be10b5e22489bb7e212a5df64b1
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-14 17:47:54 +0700

    create strock table

--- src-tauri/database/migrations/2025-11-13-084349-0000_Stock/up.sql
+++ src-tauri/database/migrations/2025-11-13-084349-0000_Stock/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+CREATE TABLE Strock (
+  strock_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  product_id INTEGER NOT NULL,
+  FORGIEN KEY (product_id) REFERENCES Product (product_id),
+  catagory VARCHAR NOT NULL,
+  satang INTEGER NOT NULL
+)
