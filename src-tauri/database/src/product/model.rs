f82245cded26f817bce67ac850aba1e3d9c54c82
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-16 18:43:44 +0700

    fixed typo

--- src-tauri/database/src/product/model.rs
+++ src-tauri/database/src/product/model.rs
@@ -1,21 +1,21 @@
 use diesel::prelude::*;
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::posts)]
+#[diesel(table_name = crate::schema::product)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Product {
     pub product_id: i32,
     pub title: String,
     pub catagory: String,
-    pub satang: i32,
+    pub satang: i32
 }
 
 #[derive(Insertable)]
-#[diesel(table_name = Product)]
+#[diesel(table_name = crate::schema::product)]
 pub struct NewProduct<'a> {
     pub title: &'a str,
     pub catagory: &'a str,
-    pub satang: &'a i32,
+    pub satang: &'a i32
 }
 
 /*
