e5183fe8052e778d18b5ef5ff2434f4c16d39fd1
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-16 18:44:01 +0700

    write insert and remove function

--- src-tauri/database/src/product/mod.rs
+++ src-tauri/database/src/product/mod.rs
@@ -1,19 +1,23 @@
 pub mod model;
 pub mod schema;
 
-use crate::model::Product;
+use crate::model::{Product,NewProduct};
+use crate::product::schema::product::product_id;
 use crate::schema::product;
 use diesel::prelude::*;
 
-pub fn InsertProduct(newprod: &NewProduct, quan: i32) {
+pub fn InsertProduct(conn:&mut SqliteConnection ,newprod: &NewProduct, quan: i32) {
     diesel::insert_into(product::table)
-        .value(&newprod)
-        .returning(newprod::as_turning())
+        .values(newprod)
+        .returning(Product::as_returning())
+        .get_result(conn)
         .expect("Error insert new product.");
 }
 
-pub fn RemoveProduct(product_id: i32) {}
+pub fn RemoveProduct(conn:&mut SqliteConnection, id: i32) {
+    diesel::delete(product::dsl::product.find(id)).execute(conn).expect("Error deleting Product");
+}
 
-pub fn UpdateProduct(prod: Product) {}
+pub fn UpdateProduct(conn:&mut SqliteConnection, prod: Product) {}
 
-pub fn FindProduct(product_id: i32) {}
+pub fn FindProduct(conn:&mut SqliteConnection, id: i32) {}
