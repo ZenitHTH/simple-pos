67ddb50c84f0855ad967a2958313ab1285f91b66
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-16 17:17:46 +0700

    write insert new product but i'm not test

--- src-tauri/database/src/product/mod.rs
+++ src-tauri/database/src/product/mod.rs
@@ -1,21 +1,19 @@
 pub mod model;
 pub mod schema;
 
+use crate::model::Product;
+use crate::schema::product;
 use diesel::prelude::*;
-use crate::model::{Product};
 
-pub fn InsertProduct(prod:Product,quan:i32){
-
+pub fn InsertProduct(newprod: &NewProduct, quan: i32) {
+    diesel::insert_into(product::table)
+        .value(&newprod)
+        .returning(newprod::as_turning())
+        .expect("Error insert new product.");
 }
 
-pub fn RemoveProduct(product_id:i32){
+pub fn RemoveProduct(product_id: i32) {}
 
-}
+pub fn UpdateProduct(prod: Product) {}
 
-pub fn UpdateProduct(prod:Product){
-
-}
-
-pub fn FindProduct(product_id:i32){
-    
-}
+pub fn FindProduct(product_id: i32) {}
