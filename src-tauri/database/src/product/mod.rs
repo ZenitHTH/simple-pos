ccbe7f15676278da746c4ffaed31912c97c9c142
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-17 16:29:39 +0700

    write basic function / find product still wrong

--- src-tauri/database/src/product/mod.rs
+++ src-tauri/database/src/product/mod.rs
@@ -1,32 +1,44 @@
 pub mod model;
 pub mod schema;
 
-use crate::model::{Product,NewProduct};
-use crate::product::schema::product::product_id;
-use crate::schema::product;
+use crate::model::{NewProduct, Product};
+use crate::schema::product as product_schema;
 use diesel::prelude::*;
+use diesel::result::Error;
 
-pub fn insert_product(conn:&mut SqliteConnection ,newprod: &NewProduct, quan: i32) {
-    diesel::insert_into(product::table)
+pub fn insert_product(conn: &mut SqliteConnection, newprod: &NewProduct) {
+    diesel::insert_into(product_schema::table)
         .values(newprod)
         .returning(Product::as_returning())
         .get_result(conn)
         .expect("Error insert new product.");
 }
 
-pub fn remove_product(conn:&mut SqliteConnection, id: i32) {
-    diesel::delete(product::dsl::product.find(id)).execute(conn).expect("Error deleting Product");
+pub fn remove_product(conn: &mut SqliteConnection, id: i32) {
+    diesel::delete(product_schema::dsl::product.find(id))
+        .execute(conn)
+        .expect("Error deleting Product");
 }
 
-pub fn update_product(conn:&mut SqliteConnection, prod: Product) {
-    use self::schema::product::dsl::{product as product_dsl,product_id,title,catagory,satang};
-    
-    let _ = diesel::update(product_dsl.find(prod.product_id)).set((
-        product_id.eq(prod.product_id)  ,
-        title.eq(prod.title),
-        catagory.eq(prod.catagory),
-        satang.eq(prod.satang)
-    )).returning(Product::as_returning()).get_result(conn).unwrap();
+pub fn update_product(conn: &mut SqliteConnection, prod: Product) {
+    use product_schema::dsl::{catagory, product as product_dsl, product_id, satang, title};
+
+    let _ = diesel::update(product_dsl.find(prod.product_id))
+        .set((
+            product_id.eq(prod.product_id),
+            title.eq(prod.title),
+            catagory.eq(prod.catagory),
+            satang.eq(prod.satang),
+        ))
+        .returning(Product::as_returning())
+        .get_result(conn)
+        .unwrap();
 }
 
-pub fn find_product(conn:&mut SqliteConnection, id: i32) {}
+pub fn find_product(conn: &mut SqliteConnection, id: i32) -> Result<Vec<Product>, Error> {
+    let prod: Vec<Product> = product_schema::table
+        .select(product_schema::product_id.eq(id))
+        .load::<Product>(conn)?;
+
+    return prod;
+}
