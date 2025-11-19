3662e32bc80f4ce700ad60c2d3ec691c9c3c7aaf
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-11-19 15:41:37 +0700

    create stock model struct

---
+++ src-tauri/database/src/Stock/model.rs
@@ -0,0 +1,24 @@
+/*
+* Stock
+   Primary ID :INT
+   Product ID : Ref Product.Primary ID
+   Quantity : INT
+*/
+
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::stock)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Stock {
+    pub stock_id: i32,
+    pub product_id: i32,
+    pub quantity: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::stock)]
+pub struct NewStock<'a> {
+    pub product_id: &'a i32,
+    pub quantity: &'a i32,
+}
