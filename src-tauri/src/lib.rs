11ff468bf0a01d83f457937b7688e7884f1cb29f
Settawat Boriruklert <settawatboriruklert@gmail.com>
2026-01-04 18:57:14 +0700

    remove old part

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,6 @@ use commands::stock::*;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             // Product Commands
             get_products,
