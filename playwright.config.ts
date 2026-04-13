bfca52863eb9e53f8b3988376a9c7a41a9667c03
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2026-04-13 17:00:59 +0700

    chore(test): refine Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e/playwright',
+  testMatch: '**/*.spec.ts',
   timeout: 60000,
-  workers: 1,
+  workers: 1, // Important: Run sequentially since they share a single Tauri instance
+  reporter: 'list',
   use: {
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 });
